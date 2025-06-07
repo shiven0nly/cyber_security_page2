747cf528e2854c1aedc78e3e3deab4af45716f78
SHIVEN <shivensharma81483@gmail.com>
2025-06-07 08:16:34 +0530

    Update icenweb.js

--- icenweb.js
+++ icenweb.js
@@ -15,12 +15,6 @@ document.getElementById('email').addEventListener('keydown', function (e) {
         document.getElementById('password').focus();
     }
 });
-document.getElementById('password').addEventListener('keydown', function (e) {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        document.getElementById('signin').focus();
-    }
-});
 
 
 document.addEventListener('DOMContentLoaded', function () {
